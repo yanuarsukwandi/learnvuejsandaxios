6a8b3cd48548128765e6b11ac522e20a5504910b
Yanuar Gandhira <yanuar.sukwandi@gmail.com>
2020-03-12 17:39:44 +0700

    Commit6

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
+    <form @submit.prevent="save">
+      <input type="text" v-model="form.title" />
+      <br />
+      <textarea cols="30" rows="10" v-model="form.content"></textarea>
+      <br />
+      <button type="submit">Save</button>
+    </form>
     <ul>
       <li v-for="article in articles" :key="article.id">
-        {{ article.title }} <br />
+        {{ article.title }}
+        <br />
         {{ article.content }}
       </li>
     </ul>
-    <button @click="Load">LoadA</button>
+    <button @click="load">Load</button>
   </div>
 </template>
 
@@ -15,18 +23,36 @@ import axios from "axios";
 export default {
   data() {
     return {
+      form: {
+        title: "",
+        content: ""
+      },
       articles: []
     };
   },
 
   async mounted() {
-    this.load();
+    // this.load();
   },
 
   methods: {
     async load() {
       const response = await axios.get("http://localhost:3000/articles");
       this.articles = response.data;
+    },
+    async save() {
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/articles",
+          this.form
+        );
+        this.articles = response.data;
+        //this.load();
+        this.title = "";
+        this.content = "";
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 };
